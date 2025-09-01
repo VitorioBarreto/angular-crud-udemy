222adc50a990617259eac6321ec154a544e5c491
VitorioBarreto <vitorio.augusto.silva@gmail.com>
2025-09-01 13:49:27 -0300

    Adição do footer e nav

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -6,17 +6,34 @@ import { App } from './app';
 import { Header } from './components/template/header/header';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+// import { provideAnimations } from '@angular/platform-browser/animations';
+import { Footer } from './components/template/footer/footer';
+import { Nav } from './components/template/nav/nav';
+
+
+import  { MatSidenavModule } from '@angular/material/sidenav'
+import  { MatCardModule } from '@angular/material/card'
+import  { MatListModule } from '@angular/material/list';
+import { Home } from './views/home/home'
+
+
 
 
 @NgModule({
   declarations: [
     App,
-    Header
+    Header,
+    Footer,
+    Nav,
+    Home
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
